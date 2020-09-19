f346e6936255e07bed90e7759e9925d03a984794
JitendraDandekar <60399471+JitendraDandekar@users.noreply.github.com>
2020-09-19 13:20:03 +0530

    modified fields

--- todo/model.py
+++ todo/model.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.utils import timezone
 
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
-    time = models.TimeField(default=timezone.now)
+    time = models.TimeField()
 
     def __str__(self):
         return self.title
